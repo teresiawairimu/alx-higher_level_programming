2ab66fb5701a1930329a2adcc0ca50c946b55012
teresiawairimu <nduatiteresia82@gmail.com>
2024-01-10 12:36:05 -0800

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     keys = list(a_dictionary.keys())
     for item in keys:
-        if isinstance(item, str):
+        #if isinstance(item, str):
             if item == key:
                 a_dictionary[item] = value
             else:
