4f03e91a96c7cda265b5de0b44477783e11fb026
teresiawairimu <nduatiteresia82@gmail.com>
2024-01-12 16:44:38 -0800

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = dict()
     for item in a_dictionary.keys():
-       a =  a_dictionary[item]
-       b = a * 2
-       new_dict[item] = b
+        a = a_dictionary[item]
+        b = a*2
+        new_dict[item] = b
     return new_dict
