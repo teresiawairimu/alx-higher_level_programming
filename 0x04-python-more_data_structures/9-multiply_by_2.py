bc80b4ee7560ad91573b0ea5c04fbce5ca81b603
teresiawairimu <nduatiteresia82@gmail.com>
2024-01-12 16:35:55 -0800

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = dict()
+    for item in a_dictionary.keys():
+       a =  a_dictionary[item]
+       b = a * 2
+       new_dict[item] = b
+    return new_dict
