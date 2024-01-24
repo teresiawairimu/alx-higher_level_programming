0eefa21952550c74030704ab29a628b811c22558
teresiawairimu <nduatiteresia82@gmail.com>
2024-01-23 16:51:50 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""A module that defines a square based on task 0
-"""
+"""A module that defines a square based on task 0"""
 
 class Square:
     """Represents a square"""
+
     def __init__(self, size):
-        """Constructor for the Square Class
-        """
+        """Constructor for the Square Class"""
         self.__size = size
