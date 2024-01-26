15f36725d97039297f2b3fc3101976daa328b0bb
teresiawairimu <nduatiteresia82@gmail.com>
2024-01-26 11:39:53 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Prints the specified number of elements on a list
+
+    Args:
+        my_list(list): A list containing the elements.
+        x(int): the integer that specifies the numbeer of elements to print
+
+    Returns:
+        The number of elements that are printed
+    """
+    number = 0
+    for element in range(x):
+        try:
+            print(my_list[element], end='')
+            number += 1
+        except IndexError:
+            break 
+    print()
+    return number
