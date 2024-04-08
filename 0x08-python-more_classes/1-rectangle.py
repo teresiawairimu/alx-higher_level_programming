df4f8a3c83451fb1d0c3d7d730d98065b3e0434b
teresiawairimu <teresiawairimu06@gmail.com>
2024-04-08 16:45:24 -0700

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A class that defines a rectangle
+"""
+class Rectangle:
+    """
+    Initialize a rectangle with default width and height values of 0
+    getter methods for retrieving both width and height
+    setter methods for setting width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+    def width(self):
+        return self.__width
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+    def height(self):
+        return self__height
+    def height(self, value):
+        if isinstance(value, int):
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
