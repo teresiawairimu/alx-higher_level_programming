14d3ae78e974eaa132ec32ae921cc80815eb7e66
teresiawairimu <teresiawairimu06@gmail.com>
2024-04-30 09:27:32 -0700

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A function that prints an integer"""
+
+
+def safe_print_integer(value):
+    """Safely prints an integer
+
+    Args:
+        value (int): The integer value to print.
+    Returns:
+        bool: True if the integer was successfully printed, False otherwise.
+    """
+
+    try:
+        formatted_number = "{:d}".format(value)
+        print(formatted_number)
+        result = True
+    except ValueError:
+        result = False
+    return result
