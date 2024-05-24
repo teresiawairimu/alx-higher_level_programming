1723a805cb843bb28dfa9420ac86e709439f2b26
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-24 11:29:57 -0700

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records of the table
+-- Results display both score and name
+-- Records should be ordered by score(top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
