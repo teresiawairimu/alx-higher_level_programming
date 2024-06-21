0ed8c92d374bdb07397ec62db20996575da8f53f
teresiawairimu <teresiawairimu06@gmail.com>
2024-06-20 17:01:52 -0700

    task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length >= 2) {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+const firstArgument = args[0];
+const secondArgument = args[1];
+console.log(`${firstArgument} is ${secondArgument}`);
