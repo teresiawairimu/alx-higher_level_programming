da5c0aeb0a19ba245907d6ccd67b4a3b389c12d3
teresiawairimu <teresiawairimu06@gmail.com>
2024-06-22 18:34:57 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Lists all states from database hbtn_0e_0_usa"""
-import MySQLdb
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class maps a table, "states"
+
+        Attributes:
+            id(int): primary key
+            name(string): name of the state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
+
 
 if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            password=mysql_password,
-            database=database_name
-            )
-    cursor = db.cursor()
-    cursor.query("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.store_result().fetch_row(maxrows=0)
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
+        f'@localhost:3306/{database_name}'
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print((state.id, state.name))
+    session.close()
