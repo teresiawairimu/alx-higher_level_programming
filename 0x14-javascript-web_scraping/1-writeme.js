64fd5733db43e4eb255fe6b76d92d86e9ae984b4
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-31 09:43:39 -0700

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+    process.exit(1);
+  }
+  console.log('sucessfully written');
+});
